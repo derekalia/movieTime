39f6e46bd27d544606138fd815dab0976afe84c3
MKS-Student <mks.student@hackreactor.com>
2017-03-07 00:30:12 -0800

    filter is working

--- js/Search.js
+++ js/Search.js
@@ -3,14 +3,33 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 class Search extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.handleSearchTerm = this.handleSearchTerm.bind(this)
+  }
+
+  handleSearchTerm (event) {
+    this.setState({searchTerm: event.target.value})
+  }
+
   render () {
     return (
       <div className='search'>
-        {preload.shows.map(item => {
-          return (
-            <ShowCard key={item.imdbID} item={item} />
-          )
-        })}
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input onChange={this.handleSearchTerm} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        {preload.shows.filter(item => {
+          return `${item.title} ${item.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+        })
+          .map(item => {
+            return (
+              <ShowCard key={item.imdbID} item={item} />
+            )
+          })}
       </div>
     )
   }
