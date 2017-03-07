cb37cac1fe47be4ef45502843aa047932655ce1a
MKS-Student <mks.student@hackreactor.com>
2017-03-06 19:22:27 -0800

    babel and webpack dev build working

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path')
 module.exports = {
-    // this is where the project is from
+  // this is where the project is from
   context: __dirname,
   entry: './js/ClientApp.js',
   devtool: 'eval',
-    // this is where we want the bundle js to go
+  // this is where we want the bundle js to go
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
+  devServer: {
+    //this is setting up the static dir
+    publicPath: '/public/'
+  },
 
-    // everytime is looks for a file it will look for the js file fitrst and then the json file with the same name
+  // everytime is looks for a file it will look for the js file fitrst and then the json file with the same name
   resolve: {
     extensions: ['.js', '.json']
   },
@@ -23,23 +27,29 @@ module.exports = {
   module: {
     rules: [
       {
-                // exclude: /node_modules/, instead of doing it like this you should build it so that it is just set to a file
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        // exclude: /node_modules/, instead of doing it like this you should build it so that it is just set to a file
         include: path.resolve(__dirname, 'js'),
 
-                // if it ends in js check it then send it to a loader
+        // if it ends in js check it then send it to a loader
         test: /\.js$/,
         loader: 'babel-loader'
       },
       {
         test: /\.css$/,
-                // if it passes the test do this array
+        // if it passes the test do this array
         use: [
           'style-loader',
           {
             loader: 'css-loader',
-                        // this is load the styles into the bundle.js file
+            // this is load the styles into the bundle.js file
             options: {
-                            // dont inline my images
+              // dont inline my images
               url: false
             }
           }
